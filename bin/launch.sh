5ae3230c205499d384f8f3e3542fcd3a36d456b6
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2022-03-07 19:49:44 +0000

    Improve launch script

--- bin/launch.sh
+++ bin/launch.sh
@@ -1,15 +1,45 @@
 #!/usr/bin/env bash
 
+
+## colours
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+LIGHT_BLUE='\033[1;34m'
+NC='\033[0m' # No Color
 DOTS="\n \033[0;32m***\033[0m"
+
+indent() {
+  sed 's/^/      /'
+}
+
+launcher() {
+    echo -e "${DOTS} ${DOTS} Launching your default browser... ${DOTS}\n"
+
+    # launch in browser
+    sleep 2
+    python -m webbrowser http://localhost:8000/
+}
+
+
 echo -e "${DOTS} ${DOTS} Firing the website up... ${DOTS}\n"
 
 # bring docker online (background)
 docker compose up -d
 
-echo -e "${DOTS} ${DOTS} Launching your default browser... ${DOTS}\n"
-
-# launch in browser
-sleep 2
-python -m webbrowser http://localhost:8000/
-
-echo "Done"
+while true; do
+  read -r -p "$(echo -e "\n\n${LIGHT_BLUE}Would you like to watch files for changes? ${NC}" | indent)" yn
+  case $yn in
+  [Yy]*)
+    launcher
+    npm run watch
+    break
+    ;;
+  [Nn]*)
+    echo -e "\nNo worries!\n\n" | indent
+    launcher
+    echo "Done"
+    break
+    ;;
+  *) echo "Please answer yes or no." | indent ;;
+  esac
+done
