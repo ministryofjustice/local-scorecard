8672ef2f86fa86030a0be1da869da60839df80ff
willmcb <william.mcbrien@digital.justice.gov.uk>
2022-03-24 11:17:21 +0000

    Add logos all but judicary

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const mix_ = require('laravel-mix');
 mix_.copy('src/index.html', 'dist/index.html')
     .copy('src/accessibility.html', 'dist/accessibility.html')
     .copy('src/judiciary-icon.png', 'dist/judiciary-icon.png')
+    .copy('src/logos', 'dist/assets/images/')
     .copy('node_modules/govuk-frontend/govuk/assets/images', 'dist/assets/images')
     .copy('node_modules/govuk-frontend/govuk/assets/fonts', 'dist/assets/fonts')
     .sass('src/app.scss', 'dist/assets/css');
