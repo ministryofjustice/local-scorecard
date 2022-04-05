20d324f6861fcc60380a9fee612fa144978a030b
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2022-04-05 14:51:14 +0100

    Move aassets

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,15 +6,17 @@ const mix_ = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix_.copy('src/*.html', 'dist')
-    .copy('src/judiciary-icon.png', 'dist/judiciary-icon.png')
+mix_.js('src/assets/app.js', 'dist/assets/js')
+    .sass('src/assets/app.scss', 'dist/assets/css')
+    .copy('src/*.html', 'dist')
+    .copy('src/assets/judiciary-icon.png', 'dist/judiciary-icon.png')
     .copy('src/logos', 'dist/assets/images/')
     .copy('src/documents', 'dist/assets/documents/')
     .copy('node_modules/govuk-frontend/govuk/assets/images', 'dist/assets/images')
     .copy('node_modules/govuk-frontend/govuk/assets/fonts', 'dist/assets/fonts')
-    .sass('src/app.scss', 'dist/assets/css');
+    .copy('node_modules/iframe-resizer/js/iframeResizer.min.js', 'dist/assets/js/iframe-resizer.min.js');
