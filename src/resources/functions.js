4813961b6009ae0150bc19deca625a46bd80f9ba
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2022-06-23 09:19:26 +0100

    Optimise JS object

--- src/resources/functions.js
+++ src/resources/functions.js
@@ -1,39 +1,19 @@
-let viz;
-const appWidth = 1225;
-let arrow = '';
-
 let app = {
+    viz: null,
+    width: 1225,
     initViz: () => {
         let containerDiv = document.getElementById('cjsData'),
             url = 'https://prod-uk-a.online.tableau.com/t/localcjsscorecard/views/TestforDamien/Introduction',
             options = {
-                width: appWidth,
+                width: app.width,
                 device: 'desktop',
                 onFirstInteractive: () => {
-                    app.interact.tab(); // resize frame for the initial loaded tab
+                    app.interact.tab(); // resize frame for initial loaded tab
                     app.listen.tab(); // listen for tab switches
                 }
             };
 
-        viz = new tableau.Viz(containerDiv, url, options);
-    },
-    current: {
-        sheet: {
-            name: null,
-            height: 0
-        }
-    },
-    get: {
-        sheet: () => viz.getWorkbook().getActiveSheet(),
-        height: () => app.get.sheet().getSize().maxSize.height,
-        name: () => app.get.sheet().getName()
-    },
-    set: {
-        name: (name) => app.current.sheet.name = name,
-        height: (height) => {
-            viz.setFrameSize(appWidth + 50, height);
-            app.current.sheet.height = height;
-        }
+        app.viz = new tableau.Viz(containerDiv, url, options);
     },
     log: (msg, value, type) => {
         let color = 'black'
@@ -60,13 +40,9 @@ let app = {
             }
         }
     },
-    listen: {
-        tab: () => {
-            viz.addEventListener(tableau.TableauEventName.TAB_SWITCH, app.interact.tab);
-        }
-    },
-    interact: {
-        tab: () => {
+    debug: {
+        resize: () => {
+            let arrow = '';
             if (app.current.sheet.name) {
                 arrow = '--> ';
                 app.log('------------');
@@ -80,8 +56,38 @@ let app = {
             app.log(
                 arrow + 'Setting iframe height to:',
                 app.get.height()
-            );
+            )
+        }
+    },
+    current: {
+        sheet: {
+            name: null,
+            height: 0
+        }
+    },
+    get: {
+        sheet: () => app.viz.getWorkbook().getActiveSheet(),
+        name: () => app.get.sheet().getName(),
+        height: () => app.get.sheet().getSize().maxSize.height
+    },
+    set: {
+        name: (name) => app.current.sheet.name = name,
+        height: (height) => {
+            app.viz.setFrameSize(app.width + 50, height);
+            app.current.sheet.height = height;
+        }
+    },
+    listen: {
+        tab: () => {
+            app.viz.addEventListener("tabswitch", app.interact.tab);
+        }
+    },
+    interact: {
+        tab: () => {
+            // drop console output
+            app.debug.resize();
 
+            //
             app.set.height( app.get.height() + 100 );
             app.set.name( app.get.name() );
         }
